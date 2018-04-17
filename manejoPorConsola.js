d86ce81cd86c7bb25ceede3664240c3efa06ea57
Uriel Espinoza <urielintemperie@gmail.com>
2018-04-17 17:57:26 -0300

    casi todo, terminalo negro forro

--- manejoPorConsola.js
+++ manejoPorConsola.js
@@ -1,30 +1,9 @@
+const UNQfy = require('./unqfy');
 
+let unqfy = new UNQfy.UNQfy();
 
-const fs = require('fs'); // necesitado para guardar/cargar unqfy
-const unqmod = require('./unqfy');
-
-// Retorna una instancia de UNQfy. Si existe filename, recupera la instancia desde el archivo.
-function getUNQfy(filename) {
-  let unqfy = new unqmod.UNQfy();
-  if (fs.existsSync(filename)) {
-    console.log();
-    unqfy = unqmod.UNQfy.load(filename);
-  }
-  return unqfy;
-}
-
-// Guarda el estado de UNQfy en filename
-function saveUNQfy(unqfy, filename) {
-  console.log();
-  unqfy.save(filename);
-}
-
-function main() {
-  let unqfy = getUNQfy("BunkerDeAdolfito");
-
-  //console.log('arguments: ');
-  //process.argv.forEach(argument => console.log(argument));
-  let params = process.argv.slice(2);
+function main(){
+    let params = process.argv.slice(2);
 
     switch (params[0]){
     //addTrack nombreAlbum nombreTrack duracionTrack listaGenerosTrack
@@ -32,20 +11,17 @@ function main() {
           //  unqfy.addTrack(params[1], {name: params[2], duration: params[3], genres: params[4]})
         //}
         case "addArtist":{
-            //addArtist unNombre unPais
             unqfy.addArtist({name: params[1], country: params[2]});
             console.log(unqfy.artistas);
             break;
         } 
 
         case "addAlbum":{
-            //addAlbum unAutor unNombre unAnio
             unqfy.addAlbum(params[1], {name: params[2], year: params[3]});
             break;
         }
 
         case "addTrack":{
-            //addTrack 
             unqfy.addTrack(params[1], {name: params[2], duration: params[3], genres: params[4]});
             break;
         }
@@ -82,29 +58,10 @@ function main() {
             break;
         }
 
-        case "getAllArtist":{
-            console.log(unqfy.artistas);
-            break;
-        }
-
-        case "getAllAlbums":{
-            console.log(unqfy.getAllAlbums());
-            break;
-        }
-
-        case "getAllTracks":{
-            console.log(unqfy.getAllTracks());
-            break;
-        }
-
         default:{
             console.log("No existe el comando dado");
         }
     }
-    
-    saveUNQfy(unqfy,"BunkerDeAdolfito");
+
 }
-
 main();
-
-
